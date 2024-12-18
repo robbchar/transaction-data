0bf00eab2c23fa526e65c002fa33b160cd95ffc6
Robb Charnock <robbchar@gmail.com>
2024-12-18 20:33:47 +0000

    adding some tests

--- src/client/src/App.test.tsx
+++ src/client/src/App.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
---
+++ src/client/src/components/Button.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Button from './Button';
+
+describe('Button component', () => {
+  const mockOnClick = vi.fn();
+
+  const renderButton = () => {
+    render(<Button onClick={mockOnClick} value="testValue" label="Test Label" />);
+  };
+
+  it('renders correctly', () => {
+    renderButton();
+    expect(screen.getByText('Test Label')).toBeInTheDocument();
+  });
+
+  it('calls onClick with correct parameters when clicked', () => {
+    renderButton();
+    const button = screen.getByText('Test Label');
+    fireEvent.click(button);
+    expect(mockOnClick).toHaveBeenCalledWith(true, 'testValue');
+  });
+
+  it('toggles isDisabled state correctly when clicked', () => {
+    renderButton();
+    const button = screen.getByText('Test Label');
+    expect(button).toHaveAttribute('data-isdisabled', 'false');
+    fireEvent.click(button);
+    expect(button).toHaveAttribute('data-isdisabled', 'true');
+    fireEvent.click(button);
+    expect(button).toHaveAttribute('data-isdisabled', 'false');
+  });
+
+  it('applies correct styles based on isDisabled state', () => {
+    renderButton();
+    const button = screen.getByText('Test Label');
+    expect(button).toHaveStyle('color: #24292E');
+    fireEvent.click(button);
+    expect(button).toHaveStyle('color: #959DA5');
+  });
+});
